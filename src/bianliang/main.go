f9f1c3e9499483f751530fb14ceb502262623f04
caoqian501 <huoshan_cq@163.com>
2020-09-05 23:55:07 +0800

    new code

---
+++ src/bianliang/main.go
@@ -0,0 +1,27 @@
+//变量的定义以及使用
+//变量的命名只能以数字字母下划线来命名，不能用数字开头
+package main
+import "fmt"
+//声明变量的标准方式
+// var name type  (var 变量名  变量类型) 
+// 变量名以关键字 var 开头，变量类型放在变量的后面，行尾无需分号
+var name string 
+var age int 
+var isOk bool
+
+//批量声明变量
+var (
+	a string
+	b int 
+	c bool
+	d float32
+)
+
+
+
+//变量的初始化
+
+
+func main(){
+	fmt.Println( 'a' )
+}
